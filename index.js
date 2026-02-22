6ebcb438fb1218aeb01fdb15dedb5e73f983d55e
Alee1450 <terzosec99@gmail.com>
2026-02-23 00:39:36 +0500

    update

--- index.js
+++ index.js
@@ -5,40 +5,47 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
 });
 
-const pendingTesters = new Set();
+const pendingUsers = new Set();
 const BLOXLINK_API_KEY = process.env.BLOXLINK_KEY;
 const GUILD_ID = "1098767779227770910";
+
+// add as many roles as you want here
+const WATCHED_ROLES = {
+  "1475214818263961701": "Tester",
+};
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('guildMemberUpdate', async (oldMember, newMember) => {
-  const testerRoleId = "1475180158989107516";
-  if (!oldMember.roles.cache.has(testerRoleId) &&
-       newMember.roles.cache.has(testerRoleId)) {
+  for (const [roleId, roleName] of Object.entries(WATCHED_ROLES)) {
+    if (!oldMember.roles.cache.has(roleId) &&
+         newMember.roles.cache.has(roleId)) {
 
-    const res = await fetch(`https://api.blox.link/v4/public/guilds/${GUILD_ID}/discord-to-roblox/${newMember.user.id}`, {
-      headers: { "Authorization": BLOXLINK_API_KEY }
-    });
+      const res = await fetch(`https://api.blox.link/v4/public/guilds/${GUILD_ID}/discord-to-roblox/${newMember.user.id}`, {
+        headers: { "Authorization": BLOXLINK_API_KEY }
+      });
 
-    const data = await res.json();
+      const data = await res.json();
 
-    if (data.robloxID) {
-      console.log(`${newMember.user.username} = Roblox ID ${data.robloxID}`);
-      pendingTesters.add(JSON.stringify({
-        discordId: newMember.user.id,
-        robloxId: data.robloxID
-      }));
-    } else {
-      console.log(`${newMember.user.username} is not verified on Bloxlink`);
+      if (data.robloxID) {
+        console.log(`${newMember.user.username} got ${roleName} = Roblox ID ${data.robloxID}`);
+        pendingUsers.add(JSON.stringify({
+          discordId: newMember.user.id,
+          robloxId: data.robloxID,
+          role: roleName
+        }));
+      } else {
+        console.log(`${newMember.user.username} is not verified on Bloxlink`);
+      }
     }
   }
 });
 
-app.get('/testers', (req, res) => {
-  res.json({ testers: [...pendingTesters].map(t => JSON.parse(t)) });
-  pendingTesters.clear();
+app.get('/users', (req, res) => {
+  res.json({ users: [...pendingUsers].map(u => JSON.parse(u)) });
+  pendingUsers.clear();
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
